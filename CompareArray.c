762035f807ec30fe657e1900f90450e0de3e9e10
manasimishra10 <manasimishra10@gmail.com>
2022-07-27 21:27:28 +0530

    Update CompareArray.c

--- CompareArray.c
+++ CompareArray.c
@@ -1,17 +1,17 @@
 // This program compares two arrays and print true if all elements are same in both arrays
 #include <stdio.h>
 #include<stdbool.h>
-int compArr (int arr1[], int arr2[]);
+bool compArr (int arr1[], int arr2[]);
 int main() {
-    int arr1[]={1,2,4,3,6};
-    int arr2[]={4,5,1,3,2};
+    int arr1[]={1,2,4,3,5};
+    int arr2[]={4,5,1,3,2,8};
     printf("array is %d\n",compArr(arr1, arr2));
     return 0;
 }
 
-int compArr (int arr1[], int arr2[]){
-        for (int i=0; i<5; ++i){
-        for (int j=i+1; j<5; ++j){
+bool compArr (int arr1[], int arr2[]){
+        for (register int i=0; i<5; ++i){
+        for (register int j=i+1; j<5; ++j){
             if (arr1[i]>arr1[j]){
                 int a= arr1[i];
                 arr1[i]=arr1[j];
@@ -19,8 +19,8 @@ int compArr (int arr1[], int arr2[]){
             }
         }
     }
-        for (int i=0; i<5; ++i){
-        for (int j=i+1; j<5; ++j){
+        for (register int i=0; i<5; ++i){
+        for (register int j=i+1; j<5; ++j){
             if (arr2[i]>arr2[j]){
                 int a= arr2[i];
                 arr2[i]=arr2[j];
@@ -28,25 +28,15 @@ int compArr (int arr1[], int arr2[]){
             }
         }
     }
-    for (int i=0;i<5; ++i){
+    for (register int i=0;i<5; ++i){
         printf("%d\n", arr1[i]);
     }
-     for (int i=0;i<5; ++i){
+     for (register int i=0;i<5; ++i){
         printf("%d\n", arr2[i]);
     }
-            int status=0;
+            bool status=0;
 
-    for (int i=0; i<5; ++i){
-        if (arr1[i]==arr2[i]){
-          status=1;  
-        }
-        else
-        status=0;
-    }
-    return status;
-}
-
-    for (int i=0; i<5; ++i){
+    for (register int i=0; i<5; ++i){
         if (arr1[i]==arr2[i]){
           status=1;  
         }
